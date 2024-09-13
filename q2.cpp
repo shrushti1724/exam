717e37c4317f9c309ef68ff52dd31c3ed82ceeb1
shrushti1724 <shrustipatel0817@gmail.com>
2024-09-13 18:57:13 +0530

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int  a,b,c;
+	
+	
+     
+     
+     if (b<=c)
+     {
+     	if(c<=a)
+     	{
+     	   
+		   	printf("b>=a")? printf("c<=a")
+     		 :printf("a>=c");
+    	}
+	 }
+	 
+}
---
+++ q3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+	int n,i;
+	printf("enter n:");
+	scanf("%d",&n);
+	
+	while(n>=10)
+	{
+	   printf("n>=10");
+	    i--;
+	}
+}
